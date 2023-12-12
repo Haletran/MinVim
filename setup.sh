6fbf7da0569301cdf035e64f4b470b179a02074f
bpasquier <bpasquier123@gmail.com>
2023-12-13 00:40:19 +0100

    Adding .vimrc wget

--- setup.sh
+++ setup.sh
@@ -12,12 +12,18 @@ if [ -f "$CONFIG_PATH" ]; then
     echo -e "Backing up your .vimrc"
     mv "$CONFIG_PATH" "$CONFIG_PATH.bak"
     echo -e "You can find your backup .vimrc at""$CONFIG_PATH.bak"
-else
-    echo "There is not a .vimrc file"
 fi
+
+#DL .vimrc file
+wget -O ~/.vimrc  https://raw.githubusercontent.com/Haletran/MinVim/main/vimrc
 
 ## Make directory for plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 
 ## Installing plugins
-#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+#PATHOGEN
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#AIRLINE
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
